964fa8eae7bd967266a656b8d52585bfcc8cb607
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-26 23:48:30 +1000

    Implemented ConsumeAction and all of its functionalities

---
+++ src/game/actions/ConsumeAction.java
@@ -0,0 +1,29 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.items.Consumable;
+
+public class ConsumeAction extends Action {
+    private Consumable consumable;
+
+    public ConsumeAction(Consumable consumable){
+        this.consumable = consumable;
+
+    }
+
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        consumable.consumedBy(actor);
+        return actor + " consumed " + consumable;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+
+        return actor + " consumes " + consumable;
+    }
+
+}
