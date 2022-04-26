09bbbfd7bf3f57d3c5100f32ac0cfcbf4a62c331
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-26 23:51:43 +1000

    Added Toad class that extends NPC Added ArrayList attribute that stores instances of Purchasable. Created constructor that adds the items that toad sells into this ArrayList

--- src/game/actors/Toad.java
+++ src/game/actors/Toad.java
@@ -1,4 +1,44 @@
 package game.actors;
 
-public class Toad {
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actions.BuyItemAction;
+import game.items.PowerStar;
+import game.items.Purchasable;
+import game.items.SuperMushroom;
+import game.items.Wrench;
+
+import java.util.ArrayList;
+
+public class Toad extends NPC{
+
+    private ArrayList<Purchasable> buyables = new ArrayList<>();
+
+    /**
+     * Constructor.
+     *
+     * @param name        the name of the Actor
+     * @param displayChar the character that will represent the Actor in the display
+     * @param hitPoints   the Actor's starting hit points
+     */
+    public Toad(String name, char displayChar, int hitPoints) {
+        super(name, displayChar, hitPoints);
+        buyables.add(new PowerStar(false));
+        buyables.add(new SuperMushroom(false));
+        buyables.add(new Wrench());
+
+        // manually add all buyable items into buyableItems
+    }
+
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        return new DoNothingAction();
+    }
+
+
+
 }
