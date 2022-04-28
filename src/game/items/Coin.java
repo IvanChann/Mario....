8e0fe5b9177f93f74348da43e4a19397f29db6ef
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-28 16:18:35 +1000

    Added Coin class that represents a coin of some value that can be picked up and converted to wallet credit

--- src/game/items/Coin.java
+++ src/game/items/Coin.java
@@ -1,4 +1,31 @@
 package game.items;
 
-public class Coin {
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.items.Item;
+import game.actions.PickupCoinAction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coin extends Item {
+    private Integer value;
+    /***
+     * Constructor.
+     *  @param value The value of the coin
+     */
+    public Coin(Integer value) {
+        super("Coin", '$', false);
+        this.value = value;
+    }
+
+
+    public List<Action> getAllowableActions() {
+        ArrayList<Action> actions = new ArrayList<>();
+        actions.add(new PickupCoinAction(this));
+        return actions;
+    }
+    public Integer getValue(){
+        return this.value;
+    }
+
 }
