8ffbf3a3142ebd0f7f245f2c83a6db62cf815b34
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-29 00:32:49 +1000

    Modified REQ4 Documentation to account for consumedBy method change

--- src/game/items/PowerStar.java
+++ src/game/items/PowerStar.java
@@ -49,7 +49,7 @@ public class PowerStar extends Item implements Consumable, Purchasable{
     }
     @Override
     public void consumedBy(Actor actor, GameMap map) {
-        if (map.locationOf(actor).getItems().size() != 0) {
+        if (map.locationOf(actor).getItems().contains(this)) {
             map.locationOf(actor).removeItem(this);
         }
         this.consumed = true;
