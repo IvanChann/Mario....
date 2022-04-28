5be69d54ce68d0334ab8a39f55ef527c6ae6dbfa
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-28 19:08:58 +1000

    Added Wrench class extending WeaponItem and implements Purchasable, has the capability to destroy shells

--- src/game/items/Wrench.java
+++ src/game/items/Wrench.java
@@ -1,4 +1,27 @@
 package game.items;
 
-public class Wrench {
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.statuses.Status;
+
+public class Wrench extends WeaponItem implements Purchasable{
+    /***
+     * Constructor.
+     */
+    public Wrench() {
+
+        super("Wrench", 'w', 50, "smashes", 80);
+        this.addCapability(Status.CAN_DESTROY_SHELL);
+    }
+
+    @Override
+    public void purchasedBy(Actor actor) {
+        actor.addItemToInventory(this);
+    }
+
+    @Override
+    public int getPrice() {
+        return 200;
+    }
 }
