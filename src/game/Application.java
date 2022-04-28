7e1da121327b6e2fa6c988284e06ca6849343db3
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-28 19:45:23 +1000

    Modified Application such that a Super Mushroom and a Power Star will be spawned near the player

--- src/game/Application.java
+++ src/game/Application.java
@@ -9,7 +9,11 @@ import edu.monash.fit2099.engine.positions.FancyGroundFactory;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
 import game.actors.Goomba;
+import game.actors.Koopa;
 import game.actors.Player;
+import game.actors.Toad;
+import game.items.PowerStar;
+import game.items.SuperMushroom;
 import game.terrain.Dirt;
 import game.terrain.Floor;
 import game.terrain.Tree;
@@ -54,8 +58,12 @@ public class Application {
 			Actor mario = new Player("Player", 'm', 100);
 			world.addPlayer(mario, gameMap.at(42, 10));
 
-			// FIXME: the Goomba should be generated from the Tree
-			gameMap.at(35, 10).addActor(new Goomba());
+			gameMap.at(42, 12).addActor(new Toad("Toad", 'o', 100));
+
+			gameMap.at(42, 13).addItem(new PowerStar(true));
+
+			gameMap.at(42, 11).addItem(new SuperMushroom(true));
+
 
 			world.run();
 
