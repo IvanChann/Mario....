db1e0f85d93be866d9d9e4541414f8da98686882
anant <asin0074@student.monash.edu>
2022-04-28 23:11:13 +1000

    Modified the Wall Class

--- src/game/terrain/Wall.java
+++ src/game/terrain/Wall.java
@@ -2,11 +2,15 @@ package game.terrain;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import game.enums.Status;
 
-public class Wall extends Ground {
+import java.nio.file.StandardCopyOption;
+
+public class Wall extends Ground implements HighGround{
 
 	public Wall() {
 		super('#');
+		this.addCapability(Status.JUMP);
 	}
 	
 	@Override
@@ -17,5 +21,24 @@ public class Wall extends Ground {
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
+	}
+
+	/**
+	 * Accessor for damage done by this ground.
+	 *
+	 * @return the damage
+	 */
+	@Override
+	public int damage() {
+		return 20;
+	}
+	/**
+	 * Accessor for success rate for each jump for this ground.
+	 *
+	 * @return the success rate.
+	 */
+	@Override
+	public double successRate() {
+		return 0.8;
 	}
 }
