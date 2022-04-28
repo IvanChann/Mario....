a624cb92782ed297fd78ee9d76bed7f971e6a34e
anant <asin0074@student.monash.edu>
2022-04-28 23:11:47 +1000

    Modified Status to add Fertile and Jump

--- src/game/enums/Status.java
+++ src/game/enums/Status.java
@@ -5,6 +5,8 @@ package game.enums;
  * It is also useful to give a `state` to abilities or actions that can be attached-detached.
  */
 public enum Status {
-    HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
+    HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy).
     TALL, // use this status to tell that current instance has "grown".
+    FERTILE, // use this to check if the ground is fertile(which is dirt).
+    JUMP, // use this status to check if the actor needs to jump.
 }
