55bdc40027fa014aaff56cd45e5dac75f9cabd53
icha0015 <icha0015@student.monash.edu>
2022-04-29 00:28:50 +1000

    Fixed reset so it can only be used once

--- src/game/actions/ResetAction.java
+++ src/game/actions/ResetAction.java
@@ -4,12 +4,14 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.reset.ResetManager;
+import game.statuses.Status;
 
 public class ResetAction extends Action {
 
     @Override
     public String execute(Actor actor, GameMap map) {
         ResetManager.getInstance().run(map);
+        actor.removeCapability(Status.CAN_RESET);
         return null;
     }
 
