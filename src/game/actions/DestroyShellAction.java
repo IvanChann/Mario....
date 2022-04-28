242e3697ed62c50969c2b4fe80cf2107722f5456
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-28 19:41:30 +1000

    Added DestroyShellAction which the constructor accepts an actor whose shell is getting destroyed (the target). This action removes the target from the map and spawns a Super Mushroom

---
+++ src/game/actions/DestroyShellAction.java
@@ -0,0 +1,26 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.items.SuperMushroom;
+
+public class DestroyShellAction extends Action {
+    private Actor target;
+
+    public DestroyShellAction(Actor target){
+        this.target = target;
+    }
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        map.locationOf(target).addItem(new SuperMushroom(true));
+        map.removeActor(target);
+        return actor + " smashes " + target + "'s shell";
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+
+        return "Destroy " + target +"'s shell!";
+    }
+}
