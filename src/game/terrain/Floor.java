ffcf1a567517521e361062d62ae64dd8fb119bd5
anant <asin0074@student.monash.edu>
2022-04-28 23:13:06 +1000

    Modified the Floor class to satisfy REQ1

--- src/game/terrain/Floor.java
+++ src/game/terrain/Floor.java
@@ -6,6 +6,8 @@ import edu.monash.fit2099.engine.positions.Ground;
  * A class that represents the floor inside a building.
  */
 public class Floor extends Ground {
+	private int age;
+
 	public Floor() {
 		super('_');
 	}
