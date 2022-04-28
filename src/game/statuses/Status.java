21a1f910573132e35977b37685d6373050b0089d
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-28 16:16:32 +1000

    Added GLOWING, CAN_DESTROY_SHELL, CANNOT_ENTER_FLOOR enums to Status

--- src/game/statuses/Status.java
+++ src/game/statuses/Status.java
@@ -5,8 +5,12 @@ package game.enums;
  * It is also useful to give a `state` to abilities or actions that can be attached-detached.
  */
 public enum Status {
-    HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy).
+    HOSTILE_TO_ENEMY, // use this status to be considered hostile towards enemy (e.g., to be attacked by enemy)
     TALL, // use this status to tell that current instance has "grown".
+    GLOWING, // effect from consuming Power Star
+    CAN_DESTROY_SHELL,
+    CANNOT_ENTER_FLOOR,
+
     FERTILE, // use this to check if the ground is fertile(which is dirt).
     JUMP, // use this status to check if the actor needs to jump.
 }
