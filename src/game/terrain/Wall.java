0b84e59879b6e590a1608f5b2e31a7ae57e3a751
Andy Ouyang <aouy0001@student.monash.edu>
2022-04-28 19:43:01 +1000

    Modified Wall to extend HighGround and no longer overrides the canActorEnter method

--- src/game/terrain/Wall.java
+++ src/game/terrain/Wall.java
@@ -2,43 +2,16 @@ package game.terrain;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
-import game.enums.Status;
 
-import java.nio.file.StandardCopyOption;
-
-public class Wall extends Ground implements HighGround{
+public class Wall extends HighGround {
 
 	public Wall() {
 		super('#');
-		this.addCapability(Status.JUMP);
 	}
-	
-	@Override
-	public boolean canActorEnter(Actor actor) {
-		return false;
-	}
+
 	
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
-	}
-
-	/**
-	 * Accessor for damage done by this ground.
-	 *
-	 * @return the damage
-	 */
-	@Override
-	public int damage() {
-		return 20;
-	}
-	/**
-	 * Accessor for success rate for each jump for this ground.
-	 *
-	 * @return the success rate.
-	 */
-	@Override
-	public double successRate() {
-		return 0.8;
 	}
 }
