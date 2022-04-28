905fdf9fbd817b77b2a05dd1057581dd2a953f9e
anant <asin0074@student.monash.edu>
2022-04-28 20:50:10 +1000

    Modified the Sprout class so it satisfies REQ1

--- src/game/terrain/Sprout.java
+++ src/game/terrain/Sprout.java
@@ -1,4 +1,29 @@
 package game.terrain;
+import game.actors.Goomba;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
 
-public class Sprout {
+public class Sprout extends Ground implements HighGround{
+    private int age;
+    public Location location;
+
+    public Sprout() {
+        super('+');
+        age = 0;
+    }
+
+    @Override
+    public void tick(Location location) {
+        age++;
+        if(getDisplayChar() != 'g') {
+            if (age==10){
+                location.setGround(new Sapling());
+            }
+            if (getDisplayChar() == '+' && Math.random()<0.1){
+                location.addActor(new Goomba());
+            }
+        }
+
+    }
 }
