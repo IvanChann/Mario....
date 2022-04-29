bcc47cef4c82c632b0e5e79d4d548b2a2250de8f
anant <asin0074@student.monash.edu>
2022-04-29 13:17:38 +1000

    Commiting High

---
+++ src/game/terrain/HighGround.java
@@ -0,0 +1,21 @@
+package game.terrain;
+
+/**
+ * Base class for all high ground's
+ */
+public interface HighGround {
+    /**
+     * The amount of damage the ground will inflict on an unsuccessful jump
+     *
+     * @return the damage, in hitpoints
+     */
+    int damage();
+
+    /**
+     * The success rate for each jump for this ground
+     *
+     * @return the success rate, in %/100
+     */
+    double successRate();
+
+}
