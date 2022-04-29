e8d34009d1e761ce9c3506922c572c9d4252be71
anant <asin0074@student.monash.edu>
2022-04-29 13:54:44 +1000

    Created a Mature class

---
+++ src/game/terrain/Mature.java
@@ -0,0 +1,60 @@
+package game.terrain;
+
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import game.actors.Koopa;
+import game.actors.Player;
+import game.enums.Status;
+
+
+public class Mature extends Ground implements HighGround{
+    public int age;
+    Exit exit;
+
+    public Mature() {
+        super('T');
+        age = 0;
+        this.addCapability(Status.JUMP);
+    }
+
+
+    @Override
+    public void tick(Location location) {
+        age++;
+        if (getDisplayChar() != 'K') {
+            if (Math.random() < 0.2) {
+                location.setGround(new Dirt());
+            }
+            if (!(location.containsAnActor()) && Math.random() < 0.15) {
+                location.addActor(new Koopa());
+            }
+        }
+
+        if (!exit.getDestination().containsAnActor() && exit.getDestination().getGround().hasCapability(Status.FERTILE)){
+            exit.getDestination().setGround(new Sprout());
+        }
+    }
+    /**
+     * Accessor for damage done by this ground.
+     *
+     * @return the damage
+     */
+    @Override
+    public int damage() {
+        return 30;
+    }
+
+    /**
+     * Accessor for success rate for each jump for this ground.
+     *
+     * @return the success rate.
+     */
+    @Override
+    public double successRate() {
+        return 0.7;
+    }
+}
+
+
