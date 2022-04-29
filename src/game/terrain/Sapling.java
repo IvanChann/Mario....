24837b9ed7cc05472b9498497e6b491a5c21ae7c
anant <asin0074@student.monash.edu>
2022-04-29 13:52:28 +1000

    Created a Sapling Class

---
+++ src/game/terrain/Sapling.java
@@ -0,0 +1,49 @@
+package game.terrain;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.enums.Status;
+
+
+public class Sapling extends Ground implements HighGround {
+    private int age;
+
+    public Sapling(){
+        super('t');
+        age = 0;
+        this.addCapability(Status.JUMP);
+    }
+
+    @Override
+    public void tick(Location location) {
+        age++;
+        if (getDisplayChar() != '$') {
+            if (age == 10) {
+                location.setGround(new Mature());
+            }
+            if (Math.random() < 0.1) {
+                location.addItem(new Coin());
+            }
+        }
+    }
+    /**
+     * Accessor for damage done by this ground.
+     *
+     * @return the damage
+     */
+    @Override
+    public int damage() {
+        return 20;
+    }
+
+    /**
+     * Accessor for success rate for each jump for this ground.
+     *
+     * @return the success rate.
+     */
+    @Override
+    public double successRate() {
+        return 0.8;
+    }
+
+}
