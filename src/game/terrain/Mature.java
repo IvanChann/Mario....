11b162d0fa93cc68c7275f00403e1c89debcebf0
icha0015 <icha0015@student.monash.edu>
2022-04-30 00:08:48 +1000

    Implemented reset for trees

--- src/game/terrain/Mature.java
+++ src/game/terrain/Mature.java
@@ -8,6 +8,7 @@ import edu.monash.fit2099.engine.positions.Exit;
 import game.actions.JumpAction;
 import game.actors.Koopa;
 import game.actors.Player;
+import game.reset.Resettable;
 import game.statuses.Status;
 
 import javax.print.attribute.standard.Destination;
@@ -15,20 +16,25 @@ import java.util.ArrayList;
 import java.util.Random;
 
 
-public class Mature extends HighGround{
+public class Mature extends HighGround implements Resettable {
     public int age;
     private int damage = 30;
     private final Random random = new Random();
+    private boolean remove = false;
 
     public Mature() {
         super('T', 30, 0.7);
-
         age = 0;
+        registerInstance();
     }
 
 
     @Override
     public void tick(Location location) {
+        if (remove){
+            location.setGround(new Dirt());
+            return;
+        }
         age++;
         if (getDisplayChar() != 'K') {
             if (Math.random() < 0.2) {
@@ -45,16 +51,18 @@ public class Mature extends HighGround{
                 if (exits.getDestination().getGround().hasCapability(Status.FERTILE)){
                     fertileGround.add(exits.getDestination());
                 }
-
-
             }
-
 
             Location randomFertile = fertileGround.get(random.nextInt(fertileGround.size()));
             randomFertile.setGround(new Sprout());
 
         }
-        }
+    }
+    @Override
+    public boolean resetInstance() {
+        return remove = random.nextBoolean();
+    }
+
 }
 
 
--- src/game/terrain/Sapling.java
+++ src/game/terrain/Sapling.java
@@ -2,20 +2,32 @@ package game.terrain;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
+import game.reset.Resettable;
 import game.statuses.Status;
 import game.items.Coin;
 
+import java.util.Random;
 
-public class Sapling extends HighGround {
+
+public class Sapling extends HighGround implements Resettable {
     private int age;
+    private boolean remove = false;
+    private final Random random = new Random();
+
 
     public Sapling(){
         super('t', 20, 0.8);
         age = 0;
+        registerInstance();
     }
 
     @Override
     public void tick(Location location) {
+        if (remove){
+            location.setGround(new Dirt());
+            return;
+        }
+
         age++;
         if (getDisplayChar() != '$') {
             if (age == 10) {
@@ -27,4 +39,8 @@ public class Sapling extends HighGround {
         }
     }
 
+    @Override
+    public boolean resetInstance() {
+        return remove = random.nextBoolean();
+    }
 }
