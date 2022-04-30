aaf477e97430d81d04d27659132f2ed90415e40f
icha0015 <icha0015@student.monash.edu>
2022-04-30 22:03:38 +1000

    Removed some lines from Wall

--- src/game/terrain/Wall.java
+++ src/game/terrain/Wall.java
@@ -1,15 +1,8 @@
 package game.terrain;
 
-import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.positions.Ground;
-import edu.monash.fit2099.engine.positions.Location;
-import game.actions.JumpAction;
-import game.statuses.Status;
-
 
 public class Wall extends HighGround {
-	HighGround highGround;
 
 	public Wall() {
 		super('#', 20, 0.8);
@@ -19,11 +12,6 @@ public class Wall extends HighGround {
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
-	}
-
-	@Override
-	public ActionList allowableActions(Actor actor, Location location, String direction) {
-		return new ActionList(new JumpAction((HighGround) location.getGround(), location, direction));
 	}
 
 	@Override
