619dbf4da0de6b94d4116c89e98e6154958821d4
icha0015 <icha0015@student.monash.edu>
2022-04-30 21:31:28 +1000

    Modified Sapling to extend from Tree

--- src/game/terrain/Sapling.java
+++ src/game/terrain/Sapling.java
@@ -1,35 +1,16 @@
 package game.terrain;
-import edu.monash.fit2099.engine.actions.ActionList;
-import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
 import game.reset.Resettable;
-import game.actions.JumpAction;
-import game.statuses.Status;
 import game.items.Coin;
 
-import java.util.Random;
-
-
-public class Sapling extends HighGround implements Resettable {
-    private int age;
-    private boolean remove = false;
-    private final Random random = new Random();
-
+public class Sapling extends Tree implements Resettable {
 
     public Sapling(){
-        super('t', 20, 0.8);
-        age = 0;
-        registerInstance();
+        super('t', 20, 0.8, 0);
     }
 
     @Override
     public void tick(Location location) {
-        if (remove){
-            location.setGround(new Dirt());
-            return;
-        }
-
         age++;
         if (getDisplayChar() != '$') {
             if (age == 10) {
@@ -39,19 +20,6 @@ public class Sapling extends HighGround implements Resettable {
                 location.addItem(new Coin(20));
             }
         }
-    }
-
-    @Override
-    public ActionList allowableActions(Actor actor, Location location, String direction) {
-        return new ActionList(new JumpAction((HighGround) location.getGround(), location, direction));
-    }
-
-    @Override
-    public boolean canActorEnter(Actor actor) {
-        return false;
-    }
-    @Override
-    public boolean resetInstance() {
-        return remove = random.nextBoolean();
+        super.tick(location);
     }
 }
