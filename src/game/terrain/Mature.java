101642e35a8548834d223662b39380fa366a7e43
icha0015 <icha0015@student.monash.edu>
2022-04-30 21:48:31 +1000

    Removed some lines in tick method in Mature

--- src/game/terrain/Mature.java
+++ src/game/terrain/Mature.java
@@ -18,15 +18,11 @@ public class Mature extends Tree implements Resettable {
     @Override
     public void tick(Location location) {
         age++;
-        if (getDisplayChar() != 'K') {
-            if (Math.random() < 0.2) {
-                location.setGround(new Dirt());
-            }
-            if (!(location.containsAnActor()) && Math.random() < 0.15) {
-                location.addActor(new Koopa());
-            }
+        if (!location.containsAnActor() && Math.random() < 0.15) {
+            location.addActor(new Koopa());
         }
-        if (age%5 == 0 && age != 0){
+
+        if (age%5 == 0){
             ArrayList<Location> fertileGround = new ArrayList<>();
             for (Exit exits : location.getExits()){
 
@@ -38,7 +34,12 @@ public class Mature extends Tree implements Resettable {
             Location randomFertile = fertileGround.get(random.nextInt(fertileGround.size()));
             randomFertile.setGround(new Sprout());
         }
+
+        if (Math.random() < 0.2) {
+            location.setGround(new Dirt());
+        }
         super.tick(location);
+
     }
 }
 
