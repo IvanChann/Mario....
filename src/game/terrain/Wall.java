906525a27d0e0212ea4a37e81aeb387a9b07f636
icha0015 <icha0015@student.monash.edu>
2022-05-02 14:27:27 +1000

    Added JavaDoc for Wall

--- src/game/terrain/Wall.java
+++ src/game/terrain/Wall.java
@@ -1,11 +1,22 @@
 package game.terrain;
 
+/**
+ * Class representing a Wall
+ */
 public class Wall extends HighGround {
 
+	/**
+	 * Constructor
+	 * @see HighGround#HighGround(char, int, double)  HighGround
+	 */
 	public Wall() {
 		super('#', 20, 0.8);
 	}
 
+	/**
+	 *
+	 * @see HighGround#blocksThrownObjects()
+	 */
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
