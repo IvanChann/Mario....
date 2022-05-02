891743d60b8ba743317a32e406f44f7260180180
icha0015 <icha0015@student.monash.edu>
2022-05-02 14:27:22 +1000

    Added JavaDoc for Tree

--- src/game/terrain/Tree.java
+++ src/game/terrain/Tree.java
@@ -5,17 +5,42 @@ import game.reset.Resettable;
 
 import java.util.Random;
 
+/**
+ * Abstract class representing a Tree
+ */
 public abstract class Tree extends HighGround implements Resettable {
+    /**
+     * Age of the Tree
+     */
     protected int age;
+
+    /**
+     * Random number generator
+     */
     protected final Random random = new Random();
+
+    /**
+     * boolean representing if the Tree should be removed from the map
+     */
     private boolean remove = false;
 
+    /**
+     * @see HighGround#HighGround(char, int, double)  HighGround
+     * @param displayChar display character
+     * @param damage the damage taken when failing jump
+     * @param successRate the successRate of jump
+     * @param age the age of the tree
+     */
     public Tree(char displayChar, int damage, double successRate, int age) {
         super(displayChar, damage, successRate);
         this.age = age;
         registerInstance();
     }
 
+    /**
+     * @see HighGround#tick(Location)
+     * @param location The location of the Ground
+     */
     @Override
     public void tick(Location location) {
         if (remove) {
@@ -24,6 +49,9 @@ public abstract class Tree extends HighGround implements Resettable {
         super.tick(location);
     }
 
+    /**
+     * @see Resettable#resetInstance()
+     */
     @Override
     public boolean resetInstance() {
         return remove = random.nextBoolean();
