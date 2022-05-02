afa253d08639ca12683a9807a3d951a04118dcb0
icha0015 <icha0015@student.monash.edu>
2022-05-02 14:06:57 +1000

    Added javaDoc for JumpAction

--- src/game/actions/JumpAction.java
+++ src/game/actions/JumpAction.java
@@ -8,17 +8,40 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
 
 
-
+/**
+ * Class representing the action of jumping to HighGround
+ */
 public class JumpAction extends Action {
-    private HighGround highGround;
-    private String direction;
-    private Location jumpLocation;
+    /**
+     * The HighGround being jumped to
+     */
+    private final HighGround highGround;
 
+    /**
+     * The direction of the HighGround
+     */
+    private final String direction;
+
+    /**
+     * The location of the HighGround
+     */
+    private final Location jumpLocation;
+
+    /**
+     * Constructor for JumpAction
+     * @param highGround HighGround being jumped to
+     * @param jumpLocation The location being jumped to
+     * @param direction The direction of the jump
+     */
     public JumpAction(HighGround highGround, Location jumpLocation, String direction){
         this.highGround = highGround;
         this.jumpLocation = jumpLocation;
         this.direction = direction;
     }
+
+    /**
+     * @see Action#execute(Actor, GameMap) 
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         if (Math.random() < highGround.getSuccessRate() || actor.hasCapability(Status.TALL)){
@@ -30,6 +53,9 @@ public class JumpAction extends Action {
         }
     }
 
+    /**
+     * @see Action#menuDescription(Actor) 
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " can jump " + highGround.getClass().getSimpleName() + " at " + direction;
