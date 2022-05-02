6e4dd1b1fc28463bf276ed819209d10d746b77c0
icha0015 <icha0015@student.monash.edu>
2022-05-02 14:27:00 +1000

    Added JavaDoc for Sapling

--- src/game/terrain/Sapling.java
+++ src/game/terrain/Sapling.java
@@ -3,12 +3,23 @@ import edu.monash.fit2099.engine.positions.Location;
 import game.reset.Resettable;
 import game.items.Coin;
 
+/**
+ * Class representing a Sapling
+ */
 public class Sapling extends Tree implements Resettable {
 
+    /**
+     * @see Tree#Tree(char, int, double, int)  Tree
+     */
     public Sapling(){
         super('t', 20, 0.8, 0);
     }
 
+    /**
+     * Grows or spawns coins based on conditions
+     * @see Tree#tick(Location)
+     * @param location The location of the Ground
+     */
     @Override
     public void tick(Location location) {
         age++;
