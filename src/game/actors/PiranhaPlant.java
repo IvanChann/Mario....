f2c9b9a8633a62ac0e2cb97e29990b6e07a4abb4
ivan <icha0015@student.monash.edu>
2022-05-11 14:36:59 +1000

    Created a PiranhaPlant class that extends from Enemy for testing purposes

---
+++ src/game/actors/PiranhaPlant.java
@@ -0,0 +1,32 @@
+package game.actors;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actions.DoNothingAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actions.AttackAction;
+
+// JUST FOR TESTING
+public class PiranhaPlant extends Enemy{
+    /**
+     * Constructor. Makes all enemies unable to enter floors by using a capability
+     *
+     */
+    public PiranhaPlant() {
+        super("Piranha Plant", 'Y', 1);
+    }
+
+    @Override
+    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        return new DoNothingAction();
+    }
+
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actions = new ActionList();
+        actions.add(new AttackAction(this, direction));
+        return actions;
+
+    }
+}
