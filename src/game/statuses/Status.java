86853d4df884272fc591364b9ef1217ea9917064
ivan <icha0015@student.monash.edu>
2022-05-11 14:35:42 +1000

    Added SPAWN_PIRANHA status to tell a WarpPipe to spawn a piranha plant

--- src/game/statuses/Status.java
+++ src/game/statuses/Status.java
@@ -13,4 +13,5 @@ public enum Status {
     CAN_RESET, // status allowing the player to reset the game
     FERTILE, // status applied to ground that marks them as fertile
     REMOVE_STAR, // status for the PowerStar class to know if the player reset their status
+    SPAWN_PIRANHA
 }
