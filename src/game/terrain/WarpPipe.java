8144e88643226d53e772fd6654d6cfcf47cd4557
ivan <icha0015@student.monash.edu>
2022-05-11 14:35:04 +1000

    Added initial implementation of WarpPipe class that extends from HighGround

---
+++ src/game/terrain/WarpPipe.java
@@ -0,0 +1,48 @@
+package game.terrain;
+
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.WarpAction;
+import game.actors.PiranhaPlant;
+import game.statuses.Status;
+
+public class WarpPipe extends HighGround{
+    /**
+     * Constructor for High Ground's
+     */
+    private Location currentLocation;
+    private WarpPipe connectingPipe;
+
+    public WarpPipe(Location currentLocation, WarpPipe connectingPipe) {
+        super('C', 0, 1);
+        this.currentLocation = currentLocation;
+        this.connectingPipe = connectingPipe;
+        this.addCapability(Status.SPAWN_PIRANHA);
+    }
+
+    @Override
+    public void tick(Location location) {
+        if (this.hasCapability(Status.SPAWN_PIRANHA)){
+            location.addActor(new PiranhaPlant());
+            this.removeCapability(Status.SPAWN_PIRANHA);
+        }
+    }
+
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
+        ActionList actions = super.allowableActions(actor, location, direction);
+        if (direction.equals("")){
+            actions.add(new WarpAction(this, this.connectingPipe));
+        }
+        return actions;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setConnectingPipe(WarpPipe connectingPipe) {
+        this.connectingPipe = connectingPipe;
+    }
+}
