1470f97e02c065263a9ee9c1b61a072b5240e643
ivan <icha0015@student.monash.edu>
2022-05-17 15:20:58 +1000

    Added the createMonologue method in PiranhaPlant to create the lines to speak

--- src/game/actors/PiranhaPlant.java
+++ src/game/actors/PiranhaPlant.java
@@ -6,6 +6,7 @@ import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.Monologue;
 import game.actions.AttackAction;
 
 // JUST FOR TESTING
@@ -20,6 +21,7 @@ public class PiranhaPlant extends Enemy{
 
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
+        super.playTurn(actions, lastAction, map, display);
         return new DoNothingAction();
     }
 
@@ -28,5 +30,11 @@ public class PiranhaPlant extends Enemy{
         actions.add(new AttackAction(this, direction));
         return actions;
 
+    }
+
+    @Override
+    public void createMonologues() {
+        monologueList.add(new Monologue("Slsstssthshs~! (Never gonna say goodbye~)"));
+        monologueList.add(new Monologue("Ohmnom nom nom nom."));
     }
 }
