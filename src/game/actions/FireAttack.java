13151ecd1ae536d3d95e1b570cd62fb521945b77
anant <asin0074@student.monash.edu>
2022-05-20 16:13:59 +1000

    Modified FireAttack to extend AttackAction and allows us to use Fire on enemy's

--- src/game/actions/FireAttack.java
+++ src/game/actions/FireAttack.java
@@ -1,23 +1,18 @@
 package game.actions;
 
-import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.items.Fire;
 import game.statuses.Status;
-import edu.monash.fit2099.engine.positions.GameMap;
 
-public class FireAttackAction extends AttackAction {
+public class FireAttack extends AttackAction {
 
     private int turns = 3;
 
-    public FireAttackAction(Actor target, String direction){
+    public FireAttack(Actor target, String direction){
         super(target, direction);
     }
 
-    public void effectsOfAttack(Actor target, Gamemap map){
-        map.locationOf(taget).addItem(new Fire(turns));
-    }
     @Override
     public String execute(Actor actor, GameMap map) {
         if (actor.hasCapability(Status.FIRE_ATTACK)){
