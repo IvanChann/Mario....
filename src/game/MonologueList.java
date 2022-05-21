357d449939fe04696259c39cf13584b01a13a4b7
icha0015 <icha0015@student.monash.edu>
2022-05-22 01:14:15 +1000

    Added JavaDoc for MonologueList

--- src/game/MonologueList.java
+++ src/game/MonologueList.java
@@ -5,17 +5,40 @@ import edu.monash.fit2099.engine.actors.Actor;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Class to store Monologues
+ */
 public class MonologueList {
+
+    /**
+     * The list of Monologues
+     */
     private ArrayList<Monologue> monologues = new ArrayList<>();
 
+    /**
+     * Random number generator
+     */
     private final Random rand = new Random();
 
+    /**
+     * Constructor
+     */
     public MonologueList(){}
 
+    /**
+     * This function returns a random Monologue for the list
+     * @return random Monologue
+     */
     public Monologue getRandom(){
         return monologues.get(rand.nextInt(monologues.size()));
     }
 
+    /**
+     * This function returns a random Monologue for the list.
+     * Checks for the Condition in otherActor
+     * @param otherActor Actor being spoken to
+     * @return random Monologue
+     */
     public Monologue getRandom(Actor otherActor){
         ArrayList<Monologue> actorMonologues = new ArrayList<>();
         for (Monologue monologue: monologues) {
@@ -26,6 +49,10 @@ public class MonologueList {
         return actorMonologues.get(rand.nextInt(actorMonologues.size()));
     }
 
+    /**
+     * Add Monologue to list
+     * @param monologue Monologue to add
+     */
     public void add(Monologue monologue) {
         monologues.add(monologue);
     }
