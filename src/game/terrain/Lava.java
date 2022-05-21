d0a9747d1e59ec09a2546c5317e22e311f8cc832
icha0015 <icha0015@student.monash.edu>
2022-05-22 01:13:36 +1000

    Added JavaDoc for Lava

--- src/game/terrain/Lava.java
+++ src/game/terrain/Lava.java
@@ -5,6 +5,9 @@ import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
 import game.statuses.Status;
 
+/**
+ * Class representing the Lava Ground type
+ */
 public class Lava extends Ground {
     /**
      * Constructor.
@@ -13,6 +16,10 @@ public class Lava extends Ground {
         super('L');
     }
 
+    /**
+     * Damages any Actor on Location
+     * @see Ground#tick(Location)
+     */
     @Override
     public void tick(Location location) {
         if (location.containsAnActor()){
@@ -24,6 +31,11 @@ public class Lava extends Ground {
         }
     }
 
+    /**
+     * Enemies cannot enter Lava
+     * @param actor the Actor to check
+     * @return boolean whether the actor can enter or not
+     */
     @Override
     public boolean canActorEnter(Actor actor) {
         return !actor.hasCapability(Status.CANNOT_ENTER_FLOOR);
