ee756939dde7d060e958b51c981efe4fd8d7d999
icha0015 <icha0015@student.monash.edu>
2022-05-22 01:14:31 +1000

    Added JavaDoc for WarpAction

--- src/game/actions/WarpAction.java
+++ src/game/actions/WarpAction.java
@@ -6,15 +6,36 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
 import game.terrain.WarpPipe;
 
+/**
+ * Class representing the Warping from a WarpPipe
+ */
 public class WarpAction extends Action {
+    /**
+     * WarpPipe being warped from
+     */
     private WarpPipe currentPipe;
+
+    /**
+     * WarpPipe being warped to
+     */
     private WarpPipe connectingPipe;
 
+    /**
+     * Constructor
+     * @param currentPipe WarpPipe being warped from
+     * @param connectingPipe WarpPipe being warped to
+     */
     public WarpAction(WarpPipe currentPipe, WarpPipe connectingPipe){
         this.currentPipe = currentPipe;
         this.connectingPipe = connectingPipe;
     }
 
+    /**
+     * @see Action#execute(Actor, GameMap)
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return line to print
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         connectingPipe.setConnectingPipe(currentPipe);
@@ -26,6 +47,11 @@ public class WarpAction extends Action {
         return actor + " warped!";
     }
 
+    /**
+     * @see Action#menuDescription(Actor)
+     * @param actor The actor performing the action.
+     * @return line to print
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " warps";
