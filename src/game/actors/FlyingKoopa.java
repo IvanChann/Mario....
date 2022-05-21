ebb075a5d95330f3ef08eda65812237308437fa6
icha0015 <icha0015@student.monash.edu>
2022-05-21 22:23:43 +1000

    Added speaking lines for FlyingKoopa

--- src/game/actors/FlyingKoopa.java
+++ src/game/actors/FlyingKoopa.java
@@ -4,6 +4,7 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.Monologue;
 import game.statuses.Status;
 
 public class FlyingKoopa extends Koopa{
@@ -16,4 +17,9 @@ public class FlyingKoopa extends Koopa{
         this.addCapability(Status.CAN_FLY);
     }
 
+    @Override
+    public void createMonologues() {
+       super.createMonologues();
+       monologueList.add(new Monologue("Pam pam pam!"));
+    }
 }
