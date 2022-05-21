b7a905beac597043cad3f519f72a6dbe7376b69a
icha0015 <icha0015@student.monash.edu>
2022-05-22 01:14:53 +1000

    Added JavaDoc for WarpPipe

--- src/game/terrain/WarpPipe.java
+++ src/game/terrain/WarpPipe.java
@@ -8,19 +8,35 @@ import game.actors.PiranhaPlant;
 import game.reset.Resettable;
 import game.statuses.Status;
 
+/**
+ * Class representing a WarpPipe
+ */
 public class WarpPipe extends HighGround implements Resettable {
+
     /**
-     * Constructor for High Ground's
+     * Location the WarpPipe is at
      */
     private Location currentLocation;
+
+    /**
+     * The connecting WarpPipe
+     */
     private WarpPipe connectingPipe;
 
+    /**
+     * Constructor
+     * @param connectingPipe The connecting WarpPipe
+     */
     public WarpPipe(WarpPipe connectingPipe) {
         super('C', 0, 1);
         this.connectingPipe = connectingPipe;
         this.addCapability(Status.SPAWN_PIRANHA);
     }
 
+    /**
+     * Spawns the Piranha plant if has the Status SPAWN_PIRANHA
+     * @param location The location of the Ground
+     */
     @Override
     public void tick(Location location) {
         this.currentLocation = location;
@@ -30,23 +46,41 @@ public class WarpPipe extends HighGround implements Resettable {
         }
     }
 
+    /**
+     * @see HighGround#allowableActions(Actor, Location, String)
+     * @param actor the Actor acting
+     * @param location the current Location
+     * @param direction the direction of the Ground from the Actor
+     * @return list of actions
+     */
     @Override
     public ActionList allowableActions(Actor actor, Location location, String direction) {
         ActionList actions = super.allowableActions(actor, location, direction);
-        if (direction.equals("")){
+        if (direction.equals("")){ // If the Actor is directly on top of the WarpPipe
             actions.add(new WarpAction(this, this.connectingPipe));
         }
         return actions;
     }
 
+    /**
+     * @return the Location of the WarpPipe
+     */
     public Location getCurrentLocation() {
         return currentLocation;
     }
 
+    /**
+     * Sets the connectingPipe
+     * This is needed as it can change based on the last Warp
+     * @param connectingPipe the connecting pipe
+     */
     public void setConnectingPipe(WarpPipe connectingPipe) {
         this.connectingPipe = connectingPipe;
     }
 
+    /**
+     * @see Resettable#resetInstance()
+     */
     @Override
     public boolean resetInstance() {
         return false;
