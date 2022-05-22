743d902beb2df0007fb7a4ff842d308e07106a88
Andy Ouyang <aouy0001@student.monash.edu>
2022-05-22 13:11:04 +1000

    Added JavaDocs for DeathMechanic

--- src/game/actions/DeathMechanic.java
+++ src/game/actions/DeathMechanic.java
@@ -6,7 +6,15 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 
+/**
+ * Class that handles the dying/dropping items mechanic for actors in the game
+ */
 public class DeathMechanic {
+    /**
+     * Method that removes actor from the map and drops their items onto the ground
+     * @param actor Actor to be removed, whose items are dropped
+     * @param map Map that the actor is on
+     */
     public static void death(Actor actor, GameMap map) {
         ActionList dropActions = new ActionList();
         // drop all items
