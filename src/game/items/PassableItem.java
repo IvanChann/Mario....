8f1cd96ad205366a0c3ea3292d086b75abd6eb89
Andy Ouyang <aouy0001@student.monash.edu>
2022-05-22 13:45:38 +1000

    Added JavaDocs for PassableItem

--- src/game/items/PassableItem.java
+++ src/game/items/PassableItem.java
@@ -10,8 +10,8 @@ public interface PassableItem {
      * Handles adding an item to the receiving actor's inventory and removing it from
      * the passing actor's inventory. Or can have custom implementations for special items
      * like Bottle
-     * @param receivingActor
-     * @param passingActor
+     * @param receivingActor Actor that is receiving the item
+     * @param passingActor Actor that is passing the item
      */
     void passItem(Actor receivingActor, Actor passingActor);
 }
