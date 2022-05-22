e42bf9d4b1bc95ae6df7e43d913ea5ce461cbc44
Andy Ouyang <aouy0001@student.monash.edu>
2022-05-22 13:21:03 +1000

    Added JavaDocs for PassItemAction

--- src/game/actions/PassItemAction.java
+++ src/game/actions/PassItemAction.java
@@ -6,14 +6,29 @@ import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.items.PassableItem;
 
+/**
+ * Class representing the action of passing items
+ */
 public class PassItemAction extends Action {
     private Actor passingActor;
     private PassableItem passedItem;
 
+    /**
+     * Constructor.
+     * @param passingActor Actor who is passing
+     * @param item Item to be passed
+     */
     public PassItemAction(Actor passingActor, PassableItem item){
         this.passingActor = passingActor;
         this.passedItem = item;
     }
+
+    /**
+     * @see Action#execute(Actor, GameMap)
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return String indicating that action has been taken
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         passedItem.passItem(actor, passingActor);
@@ -21,6 +36,11 @@ public class PassItemAction extends Action {
         return passingActor + " passed " + passedItem + " to " + actor;
     }
 
+    /**
+     * @see Action#execute(Actor, GameMap)
+     * @param actor The actor performing the action.
+     * @return String representing the action in the menu
+     */
     @Override
     public String menuDescription(Actor actor) {
 
