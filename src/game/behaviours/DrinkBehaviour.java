72c9f04aa88fd303e69cdcb81861f8dcc00cfb18
Andy Ouyang <aouy0001@student.monash.edu>
2022-05-22 13:35:25 +1000

    Added JavaDocs for DrinkBehaviour

--- src/game/behaviours/DrinkBehaviour.java
+++ src/game/behaviours/DrinkBehaviour.java
@@ -7,8 +7,18 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Ground;
 import game.statuses.Status;
 
+/**
+ * Class representing the Drink Behaviour for enemies
+ */
 public class DrinkBehaviour implements Behaviour{
 
+    /**
+     * Generates a ConsumeAction for the water in a fountain if the actor is standing on top of it
+     * @see Behaviour#getAction(Actor, GameMap)
+     * @param actor the Actor acting
+     * @param map the GameMap containing the Actor
+     * @return A ConsumeAction for the water
+     */
     @Override
     public Action getAction(Actor actor, GameMap map) {
         Ground potentialFountain = map.locationOf(actor).getGround();
