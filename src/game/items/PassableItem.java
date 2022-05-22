cba468f44e7022899144ee757e6fccb3d72a7aad
Andy Ouyang <aouy0001@student.monash.edu>
2022-05-22 13:45:24 +1000

    Added JavaDocs for PassableItem

--- src/game/items/PassableItem.java
+++ src/game/items/PassableItem.java
@@ -2,6 +2,16 @@ package game.items;
 
 import edu.monash.fit2099.engine.actors.Actor;
 
+/**
+ * Interface that all items that can be passed will implement
+ */
 public interface PassableItem {
-    public void passItem(Actor receivingActor, Actor passingActor);
+    /**
+     * Handles adding an item to the receiving actor's inventory and removing it from
+     * the passing actor's inventory. Or can have custom implementations for special items
+     * like Bottle
+     * @param receivingActor
+     * @param passingActor
+     */
+    void passItem(Actor receivingActor, Actor passingActor);
 }
