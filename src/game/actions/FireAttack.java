34662908867d97ab77d81699856fcdaf0294b130
anant <asin0074@student.monash.edu>
2022-05-22 20:53:59 +1000

    Added documentation for FireAttack class

--- src/game/actions/FireAttack.java
+++ src/game/actions/FireAttack.java
@@ -4,15 +4,26 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.items.Fire;
 import game.statuses.Status;
-
+/**
+ * Class representing the Fire Attack
+ */
 public class FireAttack extends AttackAction {
 
     private int turns = 3;
-
+    /**
+     * Constructor.
+     * @param target    the Actor to attack
+     * @param direction the direction of the target
+     */
     public FireAttack(Actor target, String direction){
         super(target, direction);
     }
-
+    /**
+     * Method used by the engine to perform the action of performing the fire attack and drops a "v" on the target ground
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return String indicating that the target has been attacked with fire
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         if (actor.hasCapability(Status.FIRE_ATTACK)){
@@ -20,7 +31,11 @@ public class FireAttack extends AttackAction {
         }
         return actor + " attacks " + target + " at " + direction + " with fire!";
     }
-
+    /**
+     * Displays the option of attacking with fire on the menu
+     * @param actor The actor performing the action.
+     * @return String indicating that player can attack with fire
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " can attack " + target + " at " + direction + " with fire!";
