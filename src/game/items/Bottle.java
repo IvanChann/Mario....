bc4da2e1285eee80b8faf0aa7b9982aa484ae485
Andy Ouyang <aouy0001@student.monash.edu>
2022-05-22 13:42:10 +1000

    Added JavaDocs for Bottle

--- src/game/items/Bottle.java
+++ src/game/items/Bottle.java
@@ -12,8 +12,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ * Class representing the Bottle that can hold Water
+ */
 public class Bottle extends Item implements PassableItem, Consumable{
 
+    /**
+     * Stack representing the different water that can be contained in the bottle
+     */
     private Stack<Water> drinks = new Stack<>();
     /***
      * Constructor.
@@ -23,14 +29,28 @@ public class Bottle extends Item implements PassableItem, Consumable{
 
     }
 
+    /**
+     * Fills bottle with water
+     * @param water Water that will be poured into the bottle
+     */
     public void fill(Water water){
         drinks.push(water);
     }
 
+    /**
+     * Checks if bottle is at a specific capacity
+     * @param capacity Capacity to be checked
+     * @return Boolean depending on whether the bottle is at the specific capacity or not
+     */
     public boolean checkCapacity(int capacity){
         return drinks.size() == capacity;
     }
 
+    /**
+     * @see PassableItem#passItem(Actor, Actor)
+     * @param receivingActor
+     * @param passingActor
+     */
     @Override
     public void passItem(Actor receivingActor, Actor passingActor) {
         BottleManager.getInstance().addBottle(receivingActor.toString(), this);
@@ -38,6 +58,10 @@ public class Bottle extends Item implements PassableItem, Consumable{
 
     }
 
+    /**
+     * Generates ConsumeAction for the water in the bottle if not empty
+     * @return List containing ConsumeAction for the water in the bottle
+     */
     @Override
     public List<Action> getAllowableActions() {
         List<Action> actions = new ArrayList<>();
@@ -52,6 +76,11 @@ public class Bottle extends Item implements PassableItem, Consumable{
         return "Bottle"  + drinks;
     }
 
+    /**
+     * @see Consumable#consumedBy(Actor, GameMap)
+     * @param actor actor who consumes the item
+     * @param map map containing the actor
+     */
     @Override
     public void consumedBy(Actor actor, GameMap map) {
         drinks.pop().consumedFromBottle(actor);
