10cc2570ecdf55b6f2c697ffdf46625c4b6f315c
anant <asin0074@student.monash.edu>
2022-05-22 20:49:56 +1000

    Added documentation for Fire class

--- src/game/items/Fire.java
+++ src/game/items/Fire.java
@@ -2,17 +2,26 @@ package game.items;
 
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.Location;
+/**
+ * Class representing Fire
+ */
 
 public class Fire  extends Item {
 
     private int turns;
 
     private final int damage = 20;
-
+    /***
+     * Constructor.
+     */
     public Fire(int turns) {
         super("Fire", 'v', false);
         this.turns = turns;
     }
+    /**
+     * Fire disappears after 3 turns and actor will be damaged if they are standing on fire
+     * @param currentLocation The location of the ground on which we lie.
+     */
 
     @Override
     public void tick(Location currentLocation) {
